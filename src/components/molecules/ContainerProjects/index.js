94410d984364ffb00a00d39e33d339d65164f3fc
Mateo Álvarez <alvarez.fing@gmail.com>
2021-09-10 22:30:54 -0300

    refactor(initial path): add a new prop to give the static path

---
+++ src/components/molecules/ContainerProjects/index.js
@@ -0,0 +1,44 @@
+import { useRedirectPage } from 'hooks/useRedirectPage'
+import PropTypes from 'prop-types'
+
+import { UiImageContain, Container, LinkTo, UIImage } from './styles'
+
+const ContainerProjects = ({
+  listOfProjects,
+  IconOfRedirectButton,
+  textOfRedirectButton,
+  pathOfRedirectProject,
+}) => {
+  const { setRedirectPage } = useRedirectPage(pathOfRedirectProject)
+
+  return (
+    <Container>
+      {listOfProjects.map(({ title, img, url }) => (
+        <UiImageContain
+          onClick={() => setRedirectPage(title)}
+          key={title}
+          title={title}
+        >
+          <LinkTo
+            target="_blank"
+            href={url}
+            onClick={(e) => e.stopPropagation()}
+          >
+            {textOfRedirectButton}
+            {IconOfRedirectButton && <IconOfRedirectButton />}
+          </LinkTo>
+          <UIImage loading="lazy" src={img} alt={title} />
+        </UiImageContain>
+      ))}
+    </Container>
+  )
+}
+
+ContainerProjects.propTypes = {
+  listOfProjects: PropTypes.arrayOf(PropTypes.object).isRequired,
+  IconOfRedirectButton: PropTypes.node,
+  textOfRedirectButton: PropTypes.string,
+  pathOfRedirectProject: PropTypes.string,
+}
+
+export default ContainerProjects
