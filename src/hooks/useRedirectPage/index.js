12480c9243419fa700e91568270c24b6afd41a34
Mateo Álvarez <alvarez.fing@gmail.com>
2021-09-10 21:45:41 -0300

    feat(redirect hook): create a custom hook for redirect pages inside

---
+++ src/hooks/useRedirectPage/index.js
@@ -0,0 +1,8 @@
+import { useLocation } from 'wouter'
+
+export const useRedirectPage = (staticPath) => {
+  const [, setLocation] = useLocation()
+  const setRedirectPage = (url) => setLocation(`/${staticPath}/${url}`)
+
+  return { setRedirectPage }
+}
