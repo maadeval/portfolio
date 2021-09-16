d46a9d855564ade950f7e55ded5d345badf0207f
Mateo Álvarez <alvarez.fing@gmail.com>
2021-09-16 18:52:30 -0300

    fix(night time): add interval between 00 and 07 am

--- src/hooks/useGetIsNight/index.js
+++ src/hooks/useGetIsNight/index.js
@@ -1,11 +1,13 @@
 export const useGetIsNight = () => {
-  const NIGHT_HOUR = 19
+  const START_NIGHT = 19
+  const FINISH_NIGHT = 7
 
   const actualTime = Date.now()
   const formatedTime = new Date(actualTime)
   const actualFormatedHour = formatedTime.getHours()
 
-  const isNight = actualFormatedHour >= NIGHT_HOUR
+  const isNight =
+    actualFormatedHour >= START_NIGHT || actualFormatedHour < FINISH_NIGHT
 
   return { isNight }
 }
