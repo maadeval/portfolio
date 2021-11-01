3b742ae76ff6df143218c1136ada8a91424ca901
Mateo Álvarez <alvarez.fing@gmail.com>
2021-10-31 21:54:24 -0300

    ref(root): change my history's text

--- src/pages/MyHistory/index.js
+++ src/pages/MyHistory/index.js
@@ -10,39 +10,75 @@ const MyHistory = () => {
     <PageContainer>
       <SEO
         title="Mi Historia"
-        description="Te cuento mi historia con la programación, soy Mateo Alvarez, a día de hoy Frontend Developer. Aunque me gusta mucho el concepto de N-SHAPE."
+        description="Te cuento mi historia con la programación, soy Mateo Alvarez, a día de hoy Frontend Developer. Aunque me gusta mucho el concepto de T-SHAPE."
       />
       <Title>Mi Historia</Title>
       <Title type={TITLE_TYPES.tertiary}>
-        Ser buena persona para mí va de la mano con ese espíritu de ayuda y de
-        colaboración en grupo. Después me di cuenta que programar en parte es
-        eso, es darlo todo entre un equipo de personas para buscar y dar
-        solución a algún problema que existe.
+        Soy desarrollador frontend con casi dos años de experiencia en sector
+        web. Soy dedicado, responsable, y relaciono mucho las capacidades
+        técnicas con las blandas, lo que significa que siempre voy a tratar de
+        ser un buen compañero de equipo, tanto dentro del proyecto en el que me
+        encuentre como fuera.
       </Title>
+
       <VerticalLine />
       <BoxSection>
-        <Title type={TITLE_TYPES.secondary}>Mi inicio como desarrollador</Title>
-        <Title type={TITLE_TYPES.tertiary}>
-          Es curioso pensar que antes de comenzar a programar, odiaba la idea de
-          hacerlo, en mi mente era como lo que muestran las películas de
-          Hollywood. Todo cambió cuando me encontraba perdido en mis estudios y
-          frustrado por el poco avance de ellas.
+        <Title type={TITLE_TYPES.secondary}>
+          Mis principios como desarrollador
         </Title>
         <Title type={TITLE_TYPES.tertiary}>
-          En búsqueda de alternativas y encontrar algo que de verdad me gustara,
-          se me dio por ver un video de Python de Pildorasinformaticas, y ahí
-          comenzó mi aventura y mi gusto por programar. Luego fui abriéndome a
-          tecnologías web y aquí me encuentro!
+          Desarrollar implica muchas cosas, algunas muy buenas, otras no tanto,
+          pero amo hacerlo, y creo que las razones por la que lo siento es
+          porque se alinea mucho a tres principios que siempre intento llevar:
+        </Title>
+        <Title type={TITLE_TYPES.tertiary}> · </Title>
+        <Title type={TITLE_TYPES.tertiary}>
+          Lo primero es resolver problemas. En mi niñez me inculcaron siempre
+          ser buena persona y ayudar. Y una de las bases éticas de la
+          programación es justamente eso mismo, colaborar entre la comunidad y
+          resolver problemas.
+        </Title>
+        <Title type={TITLE_TYPES.tertiary}> · </Title>
+        <Title type={TITLE_TYPES.tertiary}>
+          La segunda cosa es la creatividad. Antes de empezar a programar pensé
+          en estudiar carreras relacionadas al diseño, como arquitectura, diseño
+          gráfico, entre otras que tuvieran esa parte de creación. Y programar
+          te permite ser el artista de tu propio código, el traductor de una
+          idea o imagen, a algo que el usuario pueda disfrutar y usar.
+        </Title>
+        <Title type={TITLE_TYPES.tertiary}> · </Title>
+        <Title type={TITLE_TYPES.tertiary}>
+          La última cosa la definiría como la capacidad de la versatilidad. Si
+          bien el mercado y la profesión personal tienden a que te especialices
+          en algo, siempre hay avances y nuevas herramientas para usar. Y otro
+          potencial es la posibilidad de poder hacer lo mismo de muchas maneras
+          distintas. Esto no solo evita hacer tareas muy monótonas, sino buscar
+          mejores alternativas y hacer team programing para aprender nuevas
+          formas y motivar al trabajo en grupo.
         </Title>
       </BoxSection>
       <VerticalLine />
       <BoxSection>
         <Title type={TITLE_TYPES.secondary}>Stack Tecnológico</Title>
         <Title type={TITLE_TYPES.tertiary}>
-          Como camino tradicional al mundo del desarrollo web, empecé estudiando
-          HTML, CSS y JavaScript. Al tiempo me empezó a interesar lo que había
-          alrededor de ese stack, y empecé con React, tecnologías Backend y
-          diseño UI.
+          Más allá de que mi tendencia está más orientada al Frontend, considero
+          que para poder ser un buen desarrollador y un buen jugador de equipo,
+          hay que conocer el resto de tecnologías. Por eso me considero
+          T-SHAPED. ¿Qué quiere decir esto?
+        </Title>
+        <Title type={TITLE_TYPES.tertiary}>
+          T-SHAPED consiste en tener una rama principal con especialización (en
+          mi caso tecnologías Frontend acompañado de diseño UI), y una rama
+          alternativa en la que se tienen conocimientos globales que pueden
+          acompañar y complementar a la principal (en este caso, tecnologías
+          Backend con JavaScript).
+        </Title>
+        <Title type={TITLE_TYPES.tertiary}>
+          Esto, además de entender un poco más el ciclo de vida de un proyecto,
+          ayuda mucho a la comunicación entre equipos. Quiero y creo en una
+          colaboración directa entre ellos, poder opinar, valorar y contribuir
+          de la manera más libre posible. Por eso estoy abierto a aprender todo
+          lo que sea positivo para un equipo.
         </Title>
         <ListOfTecnologiesAndTitle tecnologies={listOfAllTecnologies} />
       </BoxSection>
